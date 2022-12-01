4233f67921d105fe04f9a76fb09152e77d282c86
PharmaPid <michaelboesgaard@gmail.com>
2022-12-01 08:48:32 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,5 @@
 fun main() {
     fun part1(input: List<String>): Int {
-
         var totalKcal = 0
         var highestKcal = 0
         for (kcal in input) {
@@ -13,25 +12,25 @@ fun main() {
                 val intKcal = kcal.toInt()
                 totalKcal += intKcal
             }
+
+
         }
         return highestKcal
     }
 
-    fun part2(inputStringOfKcal: String): Int {
-        return inputStringOfKcal
-                .split("\n\n")
-                .map{it
-                        .split("\n")
-                        .map{stringKcal -> stringKcal.toInt()}
-                        .reduce{totalKcal, kcal -> totalKcal + kcal}
-                }
-                .sortedByDescending{it}
-                .slice(IntRange(0,2))
-                .sum()
+    fun part2(inputString: String): Int {
+        return inputString
+            .split(Regex("\\r?\\n\\r?\\n"))
+            .map {
+                it
+                    .split(Regex("\\r?\\n"))
+                    .sumOf { kcal -> kcal.toInt() }
+            }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
-
-    val input = lineInput("Day01")
-    println(part1(input))
-    val inputText = textInput("Day01")
-    println(part2(inputText))
+    printSolutionFromInputLines("Day01", ::part1)
+    printSolutionFromInputText("Day01", ::part2)
 }
+
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,17 +1,25 @@
 import java.io.File
-import java.math.BigInteger
-import java.security.MessageDigest
+import java.nio.charset.Charset
+import kotlin.system.measureTimeMillis
 
-/**
- * Reads lines from the given input txt file.
- */
-fun lineInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput(name: String): List<String> = File("src", "$name.txt").readLines(Charset.forName("UTF-8"))
 
-fun textInput(name: String) = File("src", "$name.txt").readText()
+fun textInput(name: String): String = File("src", "$name.txt").readText(Charset.forName("UTF-8"))
 
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+fun printSolutionFromInputLines(name: String, solutionFunction: (List<String>) -> Int) {
+    var result: Int
+    val runtime = measureTimeMillis {
+        val input = readInput(name)
+        result = solutionFunction(input)
+    }
+    println("${solutionFunction.toString().substring(9..13)} execution complete. Answer: $result. Runtime: $runtime ms")
+}
+
+fun printSolutionFromInputText(name: String, solutionFunction: (String) -> Int) {
+    var result: Int
+    val runtime = measureTimeMillis {
+        val input = textInput(name)
+        result = solutionFunction(input)
+    }
+    println("${solutionFunction.toString().substring(9..13)} execution complete. Answer: $result. Runtime: $runtime ms")
+}
